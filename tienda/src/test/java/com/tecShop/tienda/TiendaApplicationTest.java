63a2c51e274ae6943d5fca1f6f93b68b8fdc9e3a
arimora13 <amoraobando@gmail.com>
2026-02-05 20:44:35 -0600

    prueba

---
+++ tienda/src/test/java/com/tecShop/tienda/TiendaApplicationTest.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.tecShop.tienda;
+
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+/**
+ *
+ * @author Arianna Mora
+ */
+
+@SpringBootTest
+public class TiendaApplicationTest {
+   @Test
+	void contextLoads() {
+	}
+ 
+}
